c79dcc8054ab0866424bcb6966aecd772e073198
davletmegahorosh <azimovdavlet460@gmail.com>
2024-09-28 21:49:36 +0600

    awdawd

--- gameapp/serializers.py
+++ gameapp/serializers.py
@@ -1,6 +1,41 @@
+from rest_framework.exceptions import ValidationError
 import base64
-from django.core.files.base import ContentFile
+import io
 from rest_framework import serializers
+
+
+class Base64FileField(serializers.FileField):
+    """
+    Serializer field for handling base64 encoded files.
+    """
+
+    def to_internal_value(self, data):
+        if isinstance(data, str):
+            try:
+                format, imgstr = data.split(';base64,')
+                ext = format.split('/')[-1]
+                data = base64.b64decode(imgstr)
+                file = io.BytesIO(data)
+                file.name = 'uploaded_file.' + ext
+                return file
+            except (TypeError, ValueError, base64.binascii.Error) as e:
+                raise ValidationError("Invalid base64 data")
+        else:
+            return super().to_internal_value(data)
+
+    def to_representation(self, value):
+        if value:
+            file = io.BytesIO(value.read())
+            return 'data:{0};base64,{1}'.format('application/octet-stream', base64.b64encode(file.getvalue()).decode())
+        return ''
+
+class FileUploadSerializer(serializers.Serializer):
+    file = Base64FileField()
+
+    def validate_file(self, value):
+        if not value.name.endswith('.py'):
+            raise serializers.ValidationError("Файл должен быть с расширением .py")
+        return value
 
 class GameSettingsSerializer(serializers.Serializer):
     map = serializers.ListField(
@@ -17,32 +52,7 @@ class GameSettingsSerializer(serializers.Serializer):
         required=True,
         help_text="Максимальное количество ходов в игре."
     )
-    file = serializers.FileField(
+    file = Base64FileField(
         required=True,
         help_text="Файл с кодом игрока для решения задачи. Должен быть .py."
     )
-
-
-class Base64FileField(serializers.FileField):
-    def to_internal_value(self, data):
-        global decoded_file
-        if isinstance(data, str):
-            if 'data:' in data and ';base64,' in data:
-                data = data.split(';base64,')[-1]
-            try:
-                decoded_file = base64.b64decode(data)
-            except TypeError:
-                self.fail('invalid_file')
-
-            file_name = 'gameapp/file.py'
-            return ContentFile(decoded_file, name=file_name)
-
-        self.fail('invalid_data')
-
-class FileUploadSerializer(serializers.Serializer):
-    file = Base64FileField()
-
-    def validate_file(self, value):
-        if not value.name.endswith('.py'):
-            raise serializers.ValidationError("Файл должен быть с расширением .py")
-        return value
